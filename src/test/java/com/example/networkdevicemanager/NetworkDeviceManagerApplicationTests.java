5c057de04d1aae86e71ac69087782647922eea76
Choi hwanjin <ghkswls04@naver.com>
2026-09-17 14:23:46 +0900

    Initial Spring Boot project

---
+++ src/test/java/com/example/networkdevicemanager/NetworkDeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networkdevicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkDeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
