5743e62e362df0bce84e307246dc788f6954d9ca
Choi hwanjin <ghkswls04@naver.com>
2026-09-17 18:52:00 +0900

    Add home endpiont

---
+++ src/main/java/com/example/networkdevicemanager/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.networkdevicemanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Network Device Manager Server";
+    }
+}
